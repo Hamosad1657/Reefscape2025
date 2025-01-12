d07525f80bf0e6980e103911ab4da84103bde2ce
Alon <alon.gal.kufsa@gmail.com>
2025-01-12 22:40:39 +0200

    Created Field constants

---
+++ src/main/kotlin/frc/robot/FieldConstants.kt
@@ -0,0 +1,43 @@
+package frc.robot
+
+import edu.wpi.first.math.geometry.Pose2d
+
+object FieldConstants {
+	object Poses {
+		val AB_CLOSE = Pose2d()
+		val AB_FAR = Pose2d()
+
+		val AB_CD_CLOSE = Pose2d()
+		val AB_CD_FAR = Pose2d()
+
+		val CD_CLOSE = Pose2d()
+		val CD_FAR = Pose2d()
+
+		val CD_EF_CLOSE = Pose2d()
+		val CD_EF_FAR = Pose2d()
+
+		val EF_CLOSE = Pose2d()
+		val EF_FAR = Pose2d()
+
+		val EF_GH_CLOSE = Pose2d()
+		val EF_GH_FAR = Pose2d()
+
+		val GH_CLOSE = Pose2d()
+		val GH_FAR = Pose2d()
+
+		val GH_IJ_CLOSE = Pose2d()
+		val GH_IJ_FAR = Pose2d()
+
+		val IJ_CLOSE = Pose2d()
+		val IJ_FAR = Pose2d()
+
+		val IJ_KL_CLOSE = Pose2d()
+		val IJ_KL_FAR = Pose2d()
+
+		val KL_CLOSE = Pose2d()
+		val KL_FAR = Pose2d()
+
+		val KL_AB_CLOSE = Pose2d()
+		val KL_AB_FAR = Pose2d()
+	}
+}
