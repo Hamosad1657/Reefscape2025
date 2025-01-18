f1c94006d7da6e1d0fbb38af0c35e29c51aaa856
Hagarcohe <hagarcohen@mevoot-eron.com>
2025-01-18 10:35:08 +0200

    feat: Changed GrabberCommands from object to a file

--- src/main/kotlin/frc/robot/commands/GrabberCommands.kt
+++ src/main/kotlin/frc/robot/commands/GrabberCommands.kt
@@ -5,62 +5,60 @@ import com.hamosad1657.lib.units.Volts
 import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
-import frc.robot.subsystems.grabber.GrabberConstants as Constants
+import frc.robot.subsystems.grabber.GrabberConstants
 import frc.robot.subsystems.grabber.GrabberSubsystem
 
-object GrabberCommands {
-	fun GrabberSubsystem.grabCommand(): Command = withName("grab") {
-		run { setWheelsVoltage(Constants.WHEELS_GRAB_VOLTAGE) } finallyDo {
-			stopWheelsMotor()
-		}
+fun GrabberSubsystem.grabCommand(): Command = withName("grab") {
+	run { setWheelsVoltage(GrabberConstants.WHEELS_GRAB_VOLTAGE) } finallyDo {
+		stopWheelsMotor()
 	}
+}
 
-	fun GrabberSubsystem.ejectCommand(): Command = withName("eject") {
-		run { setWheelsVoltage(Constants.WHEELS_EJECT_VOLTAGE) } andThen {
-			runOnce{ isCoralDetected = false }
-		} finallyDo {
-			stopWheelsMotor()
-		}
+fun GrabberSubsystem.ejectCommand(): Command = withName("eject") {
+	run { setWheelsVoltage(GrabberConstants.WHEELS_EJECT_VOLTAGE) } andThen {
+		runOnce{ isCoralDetected = false }
+	} finallyDo {
+		stopWheelsMotor()
 	}
+}
 
-	fun GrabberSubsystem.getToAngleCommand(angle: Rotation2d): Command = withName("get to angle")  {
-		run { getToAngleWithLimits(angle) } finallyDo {
-			stopAngleMotor()
-		}
+fun GrabberSubsystem.getToAngleCommand(angle: Rotation2d): Command = withName("get to angle")  {
+	run { getToAngleWithLimits(angle) } finallyDo {
+		stopAngleMotor()
 	}
+}
 
-	fun GrabberSubsystem.intakeCommand(): Command = withName("intake")	{
-		getToAngleCommand(Constants.GRABBING_ANGLE) until { isAngleWithinTolerance } alongWith grabCommand() until {
-			isCoralDetected
-		}
+fun GrabberSubsystem.intakeCommand(): Command = withName("intake")	{
+	getToAngleCommand(GrabberConstants.GRABBING_ANGLE) until { isAngleWithinTolerance } alongWith grabCommand() until {
+		isCoralDetected
 	}
+}
 
-	fun GrabberSubsystem.getReadyToPlaceCommand(angle: Rotation2d): Command = withName("get ready to place") {
-		SequentialCommandGroup(
-			intakeCommand(),
-			getToAngleCommand(angle) until { isAngleWithinTolerance }
-		) finallyDo {
-			stopAngleMotor()
-			stopWheelsMotor()
-		}
+fun GrabberSubsystem.getReadyToPlaceCommand(angle: Rotation2d): Command = withName("get ready to place") {
+	SequentialCommandGroup(
+		intakeCommand(),
+		getToAngleCommand(angle) until { isAngleWithinTolerance }
+	) finallyDo {
+		stopAngleMotor()
+		stopWheelsMotor()
 	}
+}
 
-	fun GrabberSubsystem.placeCoralCommand(angle: Rotation2d): Command = withName("place coral") {
-		SequentialCommandGroup(
-			ejectCommand() until { !isCoralDetected },
-		) finallyDo {
-			stopAngleMotor()
-			stopWheelsMotor()
-		}
+fun GrabberSubsystem.placeCoralCommand(angle: Rotation2d): Command = withName("place coral") {
+	SequentialCommandGroup(
+		ejectCommand() until { !isCoralDetected },
+	) finallyDo {
+		stopAngleMotor()
+		stopWheelsMotor()
 	}
+}
 
-	//--- Test commands ---
+//--- Test commands ---
 
-	fun GrabberSubsystem.setWheelsVoltageCommand(voltage: Volts): Command = withName("set wheels voltage") {
-		run { setWheelsVoltage(voltage) }
-	}
+fun GrabberSubsystem.setWheelsVoltageCommand(voltage: Volts): Command = withName("set wheels voltage") {
+	run { setWheelsVoltage(voltage) }
+}
 
-	fun GrabberSubsystem.setAngleMotorVoltageCommand(voltage: Volts): Command = withName("set angle motor voltage") {
-		run { setAngleVoltage(voltage) }
-	}
+fun GrabberSubsystem.setAngleMotorVoltageCommand(voltage: Volts): Command = withName("set angle motor voltage") {
+	run { setAngleVoltage(voltage) }
 }
