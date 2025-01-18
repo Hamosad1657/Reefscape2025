3ea46f9047b8612193fd74af4adfed5c9ea0f317
Alon <alon.gal.kufsa@gmail.com>
2025-01-18 17:48:56 +0200

    Applied Sufi's recommendations

--- src/main/kotlin/frc/robot/commands/IntakeCommands.kt
+++ src/main/kotlin/frc/robot/commands/IntakeCommands.kt
@@ -5,28 +5,16 @@ import com.hamosad1657.lib.units.Volts
 import frc.robot.commands.IntakeState.*
 import frc.robot.subsystems.intake.IntakeSubsystem
 
-// --- Testing commands ---
-
-/** Use for testing */
-fun IntakeSubsystem.openLoopRunWheelsCommand(voltage: Volts) = withName("Open loop run wheels") {
-	run { setWheelMotorVoltage(voltage) }
-}
-
-/** Use for testing */
-fun IntakeSubsystem.openLoopRunAngleControlCommand(voltage: Volts) = withName("Open loop angle control") {
-	run { setAngleMotorVoltage(voltage) }
-}
-
 // --- Wheels commands ---
 
 /** Runs the intaking motor so that it will intake a coral and/or drive it towards the elevator. ends instantly. */
 fun IntakeSubsystem.runMotorCommand() = withName("Run motor") {
-	runOnce { runMotor() }
+	run { runMotor() }
 }
 
 /** Runs the intaking motor in reverse so that it will move a coral in it away from the elevator. */
 fun IntakeSubsystem.runMotorReverseCommand() = withName("Run motor reverse") {
-	runOnce { runMotorReverse() }
+	run { runMotorReverse() }
 }
 
 fun IntakeSubsystem.stopMotorCommand() = withName("Stop motor") {
@@ -36,24 +24,24 @@ fun IntakeSubsystem.stopMotorCommand() = withName("Stop motor") {
 // --- Angle commands ---
 
 fun IntakeSubsystem.retractIntakeCommand() = withName("Retract intake") {
-	runOnce { retractIntake() }
+	runOnce { setAngleToRetract() }
 }
 
 fun IntakeSubsystem.deployIntakeCommand() = withName("Deploy intake") {
-	runOnce { deployIntake() }
+	runOnce { setAngleToDeploy() }
 }
 
-enum class IntakeState(val exitCondition: () -> Boolean) {
-	Deploying({
+enum class IntakeState(val shouldExitState: () -> Boolean) {
+	Deploying(shouldExitState = {
 		IntakeSubsystem.isWithinAngleTolerance
 	}),
-	Intaking({
+	Intaking(shouldExitState = {
 		IntakeSubsystem.isMotorCurrentAboveThreshold
 	}),
-	Retracting({
+	Retracting(shouldExitState = {
 		false //GrabberSubsystem.hasNote
 	}),
-	Finished({
+	Finished(shouldExitState = {
 		false
 	}),
 }
@@ -61,20 +49,34 @@ enum class IntakeState(val exitCondition: () -> Boolean) {
 fun IntakeSubsystem.intakeCommand() = withName("Intake") {
 	var intakeState = IntakeState.Deploying
 	run {
-		if (intakeState == Deploying) {
-			deployIntake()
-			if (intakeState.exitCondition()) intakeState = Intaking
-		}
-		if (intakeState == Intaking) {
-			runMotor()
-			if (intakeState.exitCondition()) intakeState = Retracting
-		}
-		if (intakeState == Retracting) {
-			retractIntake()
-			if (intakeState.exitCondition()) intakeState = Finished
-		}
-		if (intakeState == Finished) {
-			stopMotor()
+		when (intakeState) {
+			Deploying -> {
+				setAngleToDeploy()
+				if (intakeState.shouldExitState()) intakeState = Intaking
+			}
+			Intaking -> {
+				runMotor()
+				if (intakeState.shouldExitState()) intakeState = Retracting
+			}
+			Retracting -> {
+				setAngleToRetract()
+				if (intakeState.shouldExitState()) intakeState = Finished
+			}
+			Finished -> {
+				stopMotor()
+			}
 		}
 	} until { intakeState == Finished }
+}
+
+// --- Testing commands ---
+
+/** Use for testing */
+fun IntakeSubsystem.test_openLoopRunWheelsCommand(voltage: Volts) = withName("Open loop run wheels") {
+	run { setWheelMotorVoltage(voltage) }
+}
+
+/** Use for testing */
+fun IntakeSubsystem.test_openLoopRunAngleControlCommand(voltage: Volts) = withName("Open loop angle control") {
+	run { setAngleMotorVoltage(voltage) }
 }
