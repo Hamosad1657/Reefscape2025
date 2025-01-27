a3c880ab574a4d2436d9d36ab157b3adcc943c2b
Alon <alon.gal.kufsa@gmail.com>
2025-01-27 20:26:37 +0200

    Reworked CoralHandling commands to work with new code

--- src/main/kotlin/frc/robot/commands/CoralHandlingCommands.kt
+++ src/main/kotlin/frc/robot/commands/CoralHandlingCommands.kt
@@ -1,6 +1,7 @@
 package frc.robot.commands
 
 import com.hamosad1657.lib.commands.*
+import frc.robot.subsystems.elevator.joint.ElevatorJointSubsystem
 import frc.robot.subsystems.grabber.GrabberSubsystem
 import frc.robot.subsystems.intake.IntakeSubsystem
 import frc.robot.subsystems.leds.LEDsConstants.LEDsMode.GREEN_FLASH
@@ -8,13 +9,15 @@ import frc.robot.subsystems.leds.LEDsConstants.LEDsMode.YELLOW_STATIC
 import frc.robot.subsystems.leds.LEDsSubsystem
 
 fun intakeCoralFromGroundCommand() = withName("Intake coral from ground") {
-	setCoralHandlerStateCommand(CoralHandlerState.INTAKE) andThen
-		(IntakeSubsystem.intakeCommand() alongWith GrabberSubsystem.loadFromIntakeCommand()) finallyDo
+	ElevatorJointSubsystem.maintainElevatorJointStateCommand(ElevatorJointState.INTAKE) raceWith
+		(waitUntil { ElevatorJointSubsystem.isWithinTolerance } andThen
+		(IntakeSubsystem.intakeCommand() raceWith  GrabberSubsystem.loadFromIntakeCommand())) finallyDo
 		{ LEDsSubsystem.currentMode = GREEN_FLASH }
 }
 
 fun intakeCoralFromCoralStationCommand() = withName("Load coral from coral station") {
-	setCoralHandlerStateCommand(CoralHandlerState.CORAL_STATION) andThen
-		(GrabberSubsystem.loadFromCoralStationCommand() alongWith LEDsSubsystem.runOnce { LEDsSubsystem.currentMode = YELLOW_STATIC }) finallyDo
+	ElevatorJointSubsystem.maintainElevatorJointStateCommand(ElevatorJointState.CORAL_STATION) raceWith
+		(waitUntil { ElevatorJointSubsystem.isWithinTolerance } andThen
+		(GrabberSubsystem.loadFromCoralStationCommand() alongWith LEDsSubsystem.runOnce { LEDsSubsystem.currentMode = YELLOW_STATIC })) finallyDo
 		{ LEDsSubsystem.currentMode = GREEN_FLASH }
 }
