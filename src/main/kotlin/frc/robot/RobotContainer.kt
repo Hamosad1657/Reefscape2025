2c454423a2d4a1d1e2579f0356e37581f26ee9ad
Alon <alon.gal.kufsa@gmail.com>
2025-02-05 17:37:01 +0200

    Added basic robot control

--- src/main/kotlin/frc/robot/RobotContainer.kt
+++ src/main/kotlin/frc/robot/RobotContainer.kt
@@ -1,10 +1,18 @@
 package frc.robot
 
+import com.hamosad1657.lib.commands.*
 import com.hamosad1657.lib.controllers.HaCommandPS4Controller
+import com.hamosad1657.lib.units.Length
+import com.hamosad1657.lib.units.Seconds
+import com.hamosad1657.lib.units.degrees
 import edu.wpi.first.wpilibj2.command.Command
 import frc.robot.commands.*
+import frc.robot.subsystems.elevator.joint.ElevatorJointSubsystem
+import frc.robot.subsystems.grabber.GrabberSubsystem
+import frc.robot.subsystems.intake.IntakeSubsystem
 import frc.robot.subsystems.leds.LEDsConstants.LEDsMode.ACTION_FINISHED
 import frc.robot.subsystems.leds.LEDsSubsystem
+import frc.robot.subsystems.swerve.SwerveSubsystem
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -19,19 +27,55 @@ import frc.robot.subsystems.leds.LEDsSubsystem
  */
 object RobotContainer
 {
-    private const val JOYSTICK_DEADBAND = 0.02
+    private const val JOYSTICK_DEADBAND = 0.05
+    private const val EJECT_TIMEOUT: Seconds = 2.0
+
+    var elevatorJointState = ElevatorJointState.INTAKE
+    var grabberEjectMode = GrabberEjectMode.L1
+
 
     private val controllerA = HaCommandPS4Controller(JOYSTICK_DEADBAND, RobotMap.DRIVER_A_CONTROLLER_PORT)
 
     init
     {
+        configureDefaultCommands()
         configureBindings()
     }
 
-    /** Use this method to define your `trigger->command` mappings. */
+    private fun configureDefaultCommands() {
+        SwerveSubsystem.defaultCommand = SwerveSubsystem.angularVelocityDriveCommand(
+            { controllerA.leftY },
+            { controllerA.leftX },
+            { controllerA.rightX },
+            true,
+        )
+
+        ElevatorJointSubsystem.defaultCommand = ElevatorJointSubsystem.maintainElevatorJointStateCommand(ElevatorJointState.INTAKE, false)
+
+        GrabberSubsystem.defaultCommand = GrabberSubsystem.runOnce { GrabberSubsystem.stopMotor() }
+
+        IntakeSubsystem.defaultCommand = IntakeSubsystem.retractIntakeCommand()
+    }
+
     private fun configureBindings()
     {
-        controllerA.square().onTrue(intakeCoralFromGroundCommand())
+        with(controllerA) {
+            options().onTrue(SwerveSubsystem.runOnce { SwerveSubsystem.zeroGyro() })
+            share().onTrue(SwerveSubsystem.runOnce { SwerveSubsystem.setGyro(180.degrees) })
+
+            R2().whileTrue(ElevatorJointSubsystem.maintainElevatorJointStateCommand({ elevatorJointState }, useLEDs = true))
+            R1().whileTrue(GrabberSubsystem.ejectCommand{ grabberEjectMode } withTimeout(EJECT_TIMEOUT))
+
+            L1().toggleOnTrue(
+                intakeCoralFromGroundCommand() // TODO: raceWith alignToCoralCommand()
+            )
+
+            // TODO: Triangle().toggleOnTrue(alignToClosestPipeCommand())
+            square().toggleOnTrue(intakeCoralFromCoralStationCommand())
+            // TODO: Circle().whileTrue(alignToCoralStationCommand())
+
+            // TODO: Pathfinding
+        }
     }
 
     fun getAutonomousCommand(): Command?
