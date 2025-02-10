e95c567732c96b33d9026000cb02e358a9de3b5f
Alon <alon.gal.kufsa@gmail.com>
2025-02-10 19:50:49 +0200

    Corrected segment names

--- src/main/kotlin/frc/robot/autonomous/segments/EjectAlgaeIntoNetSegment.kt
+++ src/main/kotlin/frc/robot/autonomous/segments/EjectAlgaeIntoNetSegment.kt
@@ -2,6 +2,7 @@ package frc.robot.autonomous.segments
 
 import com.hamosad1657.lib.ReefSide
 import com.hamosad1657.lib.commands.*
+import com.pathplanner.lib.path.PathPlannerPath
 import edu.wpi.first.wpilibj.DriverStation.Alliance
 import edu.wpi.first.wpilibj.DriverStation.Alliance.Red
 import frc.robot.FieldConstants
@@ -13,36 +14,34 @@ import frc.robot.subsystems.leds.LEDsConstants.LEDsMode.ACTION_FINISHED
 import frc.robot.subsystems.leds.LEDsSubsystem
 import frc.robot.subsystems.swerve.SwerveSubsystem
 
-class EjectAlgaeInProcessorSegment(
+class EjectAlgaeIntoNetSegment(
 	startingSide: ReefSide,
 	endingSide: ReefSide,
 	isClockwise: Boolean,
-	): AutonomousSegment(startingSide, startingSide, isClockwise) {
-		override fun generateCommand(alliance: Alliance) = withName("eject algae in processor") {
-			// Get to Processor
-			SwerveSubsystem.alignToPoseCommand(
-				{
-					val pose = FieldConstants.Poses.AT_PROCESSOR
-					if (alliance == Red) FieldConstants.Poses.mirrorPose(pose) else pose
-				},
-				true,
-			)
-			//Tell elevator to be in state
-			ElevatorJointSubsystem.maintainElevatorJointStateCommand(
-				ElevatorJointState.PROCESSOR,
-				true
-			)
-			// Wait until elevator state is in tolerance
-			waitUntil { ElevatorJointSubsystem.isWithinTolerance } andThen
+	private val netPose: Int,
+): AutonomousSegment(startingSide, startingSide, isClockwise) {
+	override fun generateCommand(alliance: Alliance) = withName("eject algae in net ") {
+		// Get to Net Position
+		SwerveSubsystem.followPathCommand(
+			PathPlannerPath.fromPathFile("${startingSide.sideName}-far to netPosition"),
+			alliance == Alliance.Red,
+		)
+		//Tell elevator to be in state
+		ElevatorJointSubsystem.maintainElevatorJointStateCommand(
+			ElevatorJointState.NET,
+			true
+		)
+		// Wait until elevator state is in tolerance
+		waitUntil { ElevatorJointSubsystem.isWithinTolerance } andThen
 			//Eject in processor
-				(GrabberSubsystem.ejectCommand(PROCESSOR) withTimeout(2.0) finallyDo { LEDsSubsystem.currentMode = ACTION_FINISHED }) andThen
-			// Get to the endingSide
+			(GrabberSubsystem.ejectCommand(NET) withTimeout(2.0) finallyDo { LEDsSubsystem.currentMode = ACTION_FINISHED }) andThen
+			// Get to the ending pose side
 			SwerveSubsystem.alignToPoseCommand(
 				{
 					val pose = FieldConstants.Poses.FAR_POSES[endingSide.number * 2]
 					if (alliance == Red) FieldConstants.Poses.mirrorPose(pose) else pose
 				},
 				true,
-				)andThen waitUntil { ElevatorJointSubsystem.isWithinTolerance }
-		}
+			)andThen waitUntil { ElevatorJointSubsystem.isWithinTolerance }
 	}
+}
---
+++ src/main/kotlin/frc/robot/autonomous/segments/EjectAlgaeIntoProcessorSegment.kt
@@ -0,0 +1,48 @@
+package frc.robot.autonomous.segments
+
+import com.hamosad1657.lib.ReefSide
+import com.hamosad1657.lib.commands.*
+import edu.wpi.first.wpilibj.DriverStation.Alliance
+import edu.wpi.first.wpilibj.DriverStation.Alliance.Red
+import frc.robot.FieldConstants
+import frc.robot.commands.*
+import frc.robot.commands.GrabberEjectMode.*
+import frc.robot.subsystems.elevator.joint.ElevatorJointSubsystem
+import frc.robot.subsystems.grabber.GrabberSubsystem
+import frc.robot.subsystems.leds.LEDsConstants.LEDsMode.ACTION_FINISHED
+import frc.robot.subsystems.leds.LEDsSubsystem
+import frc.robot.subsystems.swerve.SwerveSubsystem
+
+class EjectAlgaeIntoProcessorSegment(
+	startingSide: ReefSide,
+	endingSide: ReefSide,
+	isClockwise: Boolean,
+	): AutonomousSegment(startingSide, startingSide, isClockwise) {
+		override fun generateCommand(alliance: Alliance) = withName("eject algae in processor") {
+			// Get to Processor
+			SwerveSubsystem.alignToPoseCommand(
+				{
+					val pose = FieldConstants.Poses.AT_PROCESSOR
+					if (alliance == Red) FieldConstants.Poses.mirrorPose(pose) else pose
+				},
+				true,
+			)
+			//Tell elevator to be in state
+			ElevatorJointSubsystem.maintainElevatorJointStateCommand(
+				ElevatorJointState.PROCESSOR,
+				true
+			)
+			// Wait until elevator state is in tolerance
+			waitUntil { ElevatorJointSubsystem.isWithinTolerance } andThen
+			//Eject in processor
+				(GrabberSubsystem.ejectCommand(PROCESSOR) withTimeout(2.0) finallyDo { LEDsSubsystem.currentMode = ACTION_FINISHED }) andThen
+			// Get to the endingSide
+			SwerveSubsystem.alignToPoseCommand(
+				{
+					val pose = FieldConstants.Poses.FAR_POSES[endingSide.number * 2]
+					if (alliance == Red) FieldConstants.Poses.mirrorPose(pose) else pose
+				},
+				true,
+				)andThen waitUntil { ElevatorJointSubsystem.isWithinTolerance }
+		}
+	}
