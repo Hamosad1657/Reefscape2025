5c95440dbf1d5f709f8fc2741dc1af3dd368cefa
Amit moses <hamosadr@gmail.com>
2025-02-11 21:15:16 +0200

    fixed names of commands

--- src/main/kotlin/frc/robot/autonomous/segments/EjectAlgaeToProcessorSegment.kt
+++ src/main/kotlin/frc/robot/autonomous/segments/EjectAlgaeToProcessorSegment.kt
@@ -1,14 +1,15 @@
 package frc.robot.autonomous.segments
 
+import com.hamosad1657.lib.ReefSide
 import com.hamosad1657.lib.commands.*
 import com.pathplanner.lib.path.PathPlannerPath
 import edu.wpi.first.wpilibj.DriverStation.Alliance
 import edu.wpi.first.wpilibj.DriverStation.Alliance.Red
+import frc.robot.FieldConstants
 import frc.robot.commands.*
 import frc.robot.commands.GrabberEjectMode.*
-import frc.robot.field.ReefSide
+import frc.robot.subsystems.elevator.joint.ElevatorJointSubsystem
 import frc.robot.subsystems.grabber.GrabberSubsystem
-import frc.robot.subsystems.jointedElevator.JointedElevatorSubsystem
 import frc.robot.subsystems.leds.LEDsConstants.LEDsMode.ACTION_FINISHED
 import frc.robot.subsystems.leds.LEDsSubsystem
 import frc.robot.subsystems.swerve.SwerveSubsystem
@@ -19,24 +20,40 @@ class EjectAlgaeToProcessorSegment(
 	isClockwise: Boolean,
 	): AutonomousSegment(startingSide, endingSide, isClockwise) {
 		override fun generateCommand(alliance: Alliance) = withName("Eject algae in processor") {
-			// Set elevator state
-			(JointedElevatorSubsystem.maintainJointedElevatorStateCommand(
-				true,
-				JointedElevatorState.PROCESSOR,
-			) raceWith(
 			// Get to Processor
 			SwerveSubsystem.followPathCommand(
-				PathPlannerPath.fromPathFile("${startingSide.name}-far to processor"),
+				PathPlannerPath.fromPathFile("${startingSide.sideName}-far to processor"),
 				alliance == Red,
-			) andThen (
+			)
+			// Correct position
+			SwerveSubsystem.alignToPoseCommand(
+				{
+					val pose = FieldConstants.Poses.AT_PROCESSOR
+					if (alliance == Red) FieldConstants.Poses.mirrorPose(pose) else pose
+				},
+				true,
+			) andThen
+			// Set elevator state
+				(ElevatorJointSubsystem.maintainElevatorJointStateCommand(
+				ElevatorJointState.PROCESSOR,
+				true
+			) raceWith(
 			// Wait until elevator state is in tolerance
-			waitUntil { JointedElevatorSubsystem.isWithinTolerance } andThen
+			waitUntil { ElevatorJointSubsystem.isWithinTolerance } andThen
 			// Eject in processor
-			(GrabberSubsystem.ejectCommand(PROCESSOR) withTimeout(1.5) finallyDo { LEDsSubsystem.currentMode = ACTION_FINISHED })) andThen
+			(GrabberSubsystem.ejectCommand(PROCESSOR) withTimeout(2.0) finallyDo { LEDsSubsystem.currentMode = ACTION_FINISHED })) andThen
 			// Get back to reef
 			SwerveSubsystem.followPathCommand(
-				PathPlannerPath.fromPathFile("Processor to ${endingSide.name}-far"),
+				PathPlannerPath.fromPathFile("processor to ${endingSide.sideName}-far"),
 				alliance == Red,
-			))) andThen waitUntil { JointedElevatorSubsystem.isWithinTolerance }
+			)
+			// correct pose
+			SwerveSubsystem.alignToPoseCommand(
+				{
+					val pose = FieldConstants.Poses.FAR_POSES[endingSide.number * 2]
+					if (alliance == Red) FieldConstants.Poses.mirrorPose(pose) else pose
+				},
+				true,
+			) andThen waitUntil { ElevatorJointSubsystem.isWithinTolerance }
 		}
 	}
