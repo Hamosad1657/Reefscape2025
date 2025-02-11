550dbf64e50cf84102d1fefc4fbdef80355e61bf
Alon <alon.gal.kufsa@gmail.com>
2025-02-11 17:44:43 +0200

    Added beam break to intake

--- src/main/kotlin/frc/robot/commands/IntakeCommands.kt
+++ src/main/kotlin/frc/robot/commands/IntakeCommands.kt
@@ -41,7 +41,7 @@ private enum class IntakeState(val shouldExitState: () -> Boolean) {
 		IntakeSubsystem.isWithinAngleTolerance
 	}),
 	Intaking(shouldExitState = {
-		IntakeSubsystem.isMotorCurrentAboveThreshold
+		IntakeSubsystem.isBeamBreakInterfered
 	}),
 	Retracting(shouldExitState = {
 		false
