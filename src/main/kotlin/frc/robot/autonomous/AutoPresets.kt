2ed1ac1a8563dd07f341b259b472d4db5c8d4de9
Alon <alon.gal.kufsa@gmail.com>
2025-02-20 15:22:42 +0200

    Updated path names and fixed presetPaths

--- src/main/kotlin/frc/robot/autonomous/AutoPresets.kt
+++ src/main/kotlin/frc/robot/autonomous/AutoPresets.kt
@@ -11,77 +11,77 @@ import frc.robot.subsystems.jointedElevator.JointedElevatorSubsystem
 import frc.robot.subsystems.swerve.SwerveSubsystem
 
 fun goTwoMetersForwardRoutine(position: Int) = withName("go two meters forward") {
-	SwerveSubsystem.followPathCommand(PathPlannerPath.fromPathFile(("position${position}Go2MetersForward")), Robot.alliance == Alliance.Red)
+	SwerveSubsystem.followInitialPathCommand(PathPlannerPath.fromPathFile(("position${position}GoForward")), Robot.alliance == Alliance.Red)
 }
 
 fun scoreL1GHLeftTopRoutine() = withName("L1 GH left top routine") {
 	JointedElevatorSubsystem.maintainJointedElevatorStateCommand(true, JointedElevatorState.L1) raceWith
-		( SwerveSubsystem.followPathCommand(PathPlannerPath.fromPathFile("GHL1ScoreLeft"), Robot.alliance == Alliance.Red) andThen
+		( SwerveSubsystem.followInitialPathCommand(PathPlannerPath.fromPathFile("GHL1ScoreLeft"), Robot.alliance == Alliance.Red) andThen
 			(GrabberSubsystem.setVoltageCommand(true, EJECT_TO_L1) withTimeout(3.0)))
 }
 
 fun scoreL1GHRightTopRoutine() = withName("L1 GH right top routine") {
 	JointedElevatorSubsystem.maintainJointedElevatorStateCommand(true, JointedElevatorState.L1) raceWith
-		( SwerveSubsystem.followPathCommand(PathPlannerPath.fromPathFile("GHL1ScoreRight"), Robot.alliance == Alliance.Red) andThen
+		( SwerveSubsystem.followInitialPathCommand(PathPlannerPath.fromPathFile("GHL1ScoreRight"), Robot.alliance == Alliance.Red) andThen
 			(GrabberSubsystem.setVoltageCommand(true, EJECT_TO_L1) withTimeout(3.0)))
 }
 
 fun scoreL4GHLeftTopRoutine() = withName("L4 GH left top routine") {
 	JointedElevatorSubsystem.maintainJointedElevatorStateCommand(true, JointedElevatorState.L4) raceWith
-		( SwerveSubsystem.followPathCommand(PathPlannerPath.fromPathFile("GHL4ScoreLeft"), Robot.alliance == Alliance.Red) andThen
-			(GrabberSubsystem.setVoltageCommand(true, EJECT_TO_L1) withTimeout(3.0)))
+		( SwerveSubsystem.followInitialPathCommand(PathPlannerPath.fromPathFile("GHL4ScoreLeft"), Robot.alliance == Alliance.Red) andThen
+			(GrabberSubsystem.setVoltageCommand(true, EJECT_TO_L4) withTimeout(3.0)))
 }
 
 fun scoreL4GHRightTopRoutine() = withName("L4 GH right top routine") {
 	JointedElevatorSubsystem.maintainJointedElevatorStateCommand(true, JointedElevatorState.L4) raceWith
-		( SwerveSubsystem.followPathCommand(PathPlannerPath.fromPathFile("GHL4ScoreRight"), Robot.alliance == Alliance.Red) andThen
-			(GrabberSubsystem.setVoltageCommand(true, EJECT_TO_L1) withTimeout(3.0)))
+		( SwerveSubsystem.followInitialPathCommand(PathPlannerPath.fromPathFile("GHL4ScoreRight"), Robot.alliance == Alliance.Red) andThen
+			(GrabberSubsystem.setVoltageCommand(true, EJECT_TO_L4) withTimeout(3.0)))
 }
 
 fun scoreL1IJLeftTopRoutinePosition6() = withName("L1 IJ left Position6 routine") {
 	JointedElevatorSubsystem.maintainJointedElevatorStateCommand(true, JointedElevatorState.L1) raceWith
-		( SwerveSubsystem.followPathCommand(PathPlannerPath.fromPathFile("Position6IJL1ScoreLeft"), Robot.alliance == Alliance.Red) andThen
+		( SwerveSubsystem.followInitialPathCommand(PathPlannerPath.fromPathFile("Position6IJL1ScoreLeft"), Robot.alliance == Alliance.Red) andThen
 			(GrabberSubsystem.setVoltageCommand(true, EJECT_TO_L1) withTimeout(3.0)))
 }
 
 fun scoreL1IJRightTopRoutinePosition6() = withName("L1 IJ right Position6 routine") {
 	JointedElevatorSubsystem.maintainJointedElevatorStateCommand(true, JointedElevatorState.L1) raceWith
-		( SwerveSubsystem.followPathCommand(PathPlannerPath.fromPathFile("Position6IJL1ScoreLeft"), Robot.alliance == Alliance.Red) andThen
+		( SwerveSubsystem.followInitialPathCommand(PathPlannerPath.fromPathFile("Position6IJL1ScoreLeft"), Robot.alliance == Alliance.Red) andThen
 			(GrabberSubsystem.setVoltageCommand(true, EJECT_TO_L1) withTimeout(3.0)))
 }
 
 fun scoreL1IJLeftTopRoutinePosition5() = withName("L1 IJ left Position5 routine") {
 	JointedElevatorSubsystem.maintainJointedElevatorStateCommand(true, JointedElevatorState.L1) raceWith
-		( SwerveSubsystem.followPathCommand(PathPlannerPath.fromPathFile("Position5IJL1ScoreLeft"), Robot.alliance == Alliance.Red) andThen
+		( SwerveSubsystem.followInitialPathCommand(PathPlannerPath.fromPathFile("Position5IJL1ScoreLeft"), Robot.alliance == Alliance.Red) andThen
 			(GrabberSubsystem.setVoltageCommand(true, EJECT_TO_L1) withTimeout(3.0)))
 }
 
 fun scoreL1IJRightTopRoutinePosition5() = withName("L1 EF right Position5 routine") {
 	JointedElevatorSubsystem.maintainJointedElevatorStateCommand(true, JointedElevatorState.L1) raceWith
-		( SwerveSubsystem.followPathCommand(PathPlannerPath.fromPathFile("Position5IJL1ScoreLeft"), Robot.alliance == Alliance.Red) andThen
+		( SwerveSubsystem.followInitialPathCommand(PathPlannerPath.fromPathFile("Position5IJL1ScoreLeft"), Robot.alliance == Alliance.Red) andThen
 			(GrabberSubsystem.setVoltageCommand(true, EJECT_TO_L1) withTimeout(3.0)))
 }
 
 fun scoreL1EFLeftTopRoutinePosition2() = withName("L1 EF left Position2 routine") {
 	JointedElevatorSubsystem.maintainJointedElevatorStateCommand(true, JointedElevatorState.L1) raceWith
-		( SwerveSubsystem.followPathCommand(PathPlannerPath.fromPathFile("Position1EFL1ScoreLeft"), Robot.alliance == Alliance.Red) andThen
+		( SwerveSubsystem.followInitialPathCommand(PathPlannerPath.fromPathFile("Position1EFL1ScoreLeft"), Robot.alliance == Alliance.Red) andThen
 			(GrabberSubsystem.setVoltageCommand(true, EJECT_TO_L1) withTimeout(3.0)))
 }
 
 fun scoreL1EFRightTopRoutinePosition2() = withName("L1 EF right Position2 routine") {
 	JointedElevatorSubsystem.maintainJointedElevatorStateCommand(true, JointedElevatorState.L1) raceWith
-		( SwerveSubsystem.followPathCommand(PathPlannerPath.fromPathFile("Position1EFL1ScoreLeft"), Robot.alliance == Alliance.Red) andThen
+		( SwerveSubsystem.followInitialPathCommand(PathPlannerPath.fromPathFile("Position1EFL1ScoreLeft"), Robot.alliance == Alliance.Red) andThen
 			(GrabberSubsystem.setVoltageCommand(true, EJECT_TO_L1) withTimeout(3.0)))
 }
 
 fun scoreL1EFLeftTopRoutinePosition1() = withName("L1 EF left Position1 routine") {
 	JointedElevatorSubsystem.maintainJointedElevatorStateCommand(true, JointedElevatorState.L1) raceWith
-		( SwerveSubsystem.followPathCommand(PathPlannerPath.fromPathFile("Position1EFL1ScoreLeft"), Robot.alliance == Alliance.Red) andThen
+		( SwerveSubsystem.followInitialPathCommand(PathPlannerPath.fromPathFile("Position1EFL1ScoreLeft"), Robot.alliance == Alliance.Red) andThen
 			(GrabberSubsystem.setVoltageCommand(true, EJECT_TO_L1) withTimeout(3.0)))
 }
 
 fun scoreL1EFRightTopRoutinePosition1() = withName("L1 EF right Position1 routine") {
 	JointedElevatorSubsystem.maintainJointedElevatorStateCommand(true, JointedElevatorState.L1) raceWith
-		( SwerveSubsystem.followPathCommand(PathPlannerPath.fromPathFile("Position1EFL1ScoreLeft"), Robot.alliance == Alliance.Red) andThen
+		( SwerveSubsystem.followInitialPathCommand(PathPlannerPath.fromPathFile("Position1EFL1ScoreLeft"), Robot.alliance == Alliance.Red) andThen
 			(GrabberSubsystem.setVoltageCommand(true, EJECT_TO_L1) withTimeout(3.0)))
 }
