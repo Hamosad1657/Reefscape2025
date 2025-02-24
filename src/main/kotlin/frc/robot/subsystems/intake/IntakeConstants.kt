2122666f075711dc001c0403f83800b9da7acd39
Alon <alon.gal.kufsa@gmail.com>
2025-02-24 15:13:44 +0200

    Tuned ejecting voltage

--- src/main/kotlin/frc/robot/subsystems/intake/IntakeConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/IntakeConstants.kt
@@ -26,7 +26,7 @@ object IntakeConstants {
 
 	const val INTAKING_VOLTAGE: Volts = 8.5
 
-	const val EJECTING_VOLTAGE: Volts = -5.0
+	const val EJECTING_VOLTAGE: Volts = -11.0
 
 	/** Works in radians. */
 	val ANGLE_PID_GAINS = PIDGains(
