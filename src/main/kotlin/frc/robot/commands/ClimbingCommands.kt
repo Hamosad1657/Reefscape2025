bb852e8ef15c70eec26e674affd7017d8e9a3972
Hagarcohe <hagarcohen@mevoot-eron.com>
2025-03-02 18:48:23 +0200

    made changes to reflect changes in sketch

--- src/main/kotlin/frc/robot/commands/ClimbingCommands.kt
+++ src/main/kotlin/frc/robot/commands/ClimbingCommands.kt
@@ -1,15 +1,21 @@
 package frc.robot.commands
 
 import com.hamosad1657.lib.commands.*
-import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.wpilibj2.command.Command
+import frc.robot.subsystems.climbing.ClimbingConstants.EXTENDING_VOLTAGE
+import frc.robot.subsystems.climbing.ClimbingConstants.RETRACTING_VOLTAGE
 import frc.robot.subsystems.climbing.ClimbingSubsystem
 import frc.robot.subsystems.climbing.ClimbingConstants as Constants
 
-fun ClimbingSubsystem.grabCage(): Command = withName("climb") {
-	run { setWheelsMotorVoltage(Constants.GRABBING_CAGE_VOLTAGE) } withTimeout (1.0)
+fun ClimbingSubsystem.grabCage(): Command = withName("Grab cage") {
+	run { setClimbingMotorVoltage(Constants.CAGE_GRABBING_VOLTAGE) } withTimeout (1.0)
 }
 
-fun ClimbingSubsystem.setClimberAngle(setpoint: Rotation2d): Command = withName("climb") {
-	run { setAngleMotorSetpoint(setpoint) } until { isWithinAngleTolerance }
+fun ClimbingSubsystem.extendClimbingSystem(): Command = withName("extend ") {
+	run { setAngleMotorVoltage(EXTENDING_VOLTAGE) }
 }
+
+fun ClimbingSubsystem.retractClimbingSystem(): Command = withName("extend ") {
+	run { setAngleMotorVoltage(RETRACTING_VOLTAGE) }
+}
+
--- src/main/kotlin/frc/robot/subsystems/climbing/ClimbingSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/climbing/ClimbingSubsystem.kt
@@ -1,9 +1,7 @@
 package frc.robot.subsystems.climbing
 
-import com.ctre.phoenix6.hardware.CANcoder
 import com.hamosad1657.lib.motors.HaSparkFlex
 import com.hamosad1657.lib.units.Volts
-import com.hamosad1657.lib.units.degrees
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode
 import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.util.sendable.SendableBuilder
@@ -11,13 +9,12 @@ import edu.wpi.first.wpilibj.Alert.AlertType
 import edu.wpi.first.wpilibj.DriverStation
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import frc.robot.RobotMap
-import kotlin.math.absoluteValue
 import frc.robot.subsystems.climbing.ClimbingConstants as Constants
 
 object ClimbingSubsystem: SubsystemBase() {
 	// --- Motors ---
 
-	private val wheelsMotor = HaSparkFlex(RobotMap.Climbing.WHEELS_MOTOR_ID).apply {
+	private val climbingMotor = HaSparkFlex(RobotMap.Climbing.WHEELS_MOTOR_ID).apply {
 		IdleMode.kBrake
 	}
 
@@ -25,53 +22,12 @@ object ClimbingSubsystem: SubsystemBase() {
 		IdleMode.kBrake
 	}
 
-	private val angleEncoder = CANcoder(RobotMap.Climbing.CAN_CODER_ID).apply {
-		configurator.apply(Constants.CAN_CODER_CONFIGS)
-	}
-
-	private val anglePIDController = Constants.ANGLE_PID_GAINS.toPIDController()
-
-	// --- State Getters ---
-	private var angleSetpoint: Rotation2d = 0.0.degrees
-	private val currentAngle: Rotation2d get() = Rotation2d.fromRotations(angleEncoder.position.valueAsDouble) + Constants.CANCODER_OFFSET
-	private val angleError: Rotation2d get() = angleSetpoint - currentAngle
-	val isWithinAngleTolerance get() = angleError.degrees.absoluteValue <= Constants.angleTolerance.degrees
-
 	// --- Functions ---
-	private fun calculateAngleMotorFF(): Volts = currentAngle.cos * Constants.ANGLE_KG
-
-	private fun isMovingTowardsAngleLimits(output: Double) =
-		(output <= 0.0) ||
-		(output >= 0.0)
+	fun setClimbingMotorVoltage(voltage: Volts) {
+		climbingMotor.setVoltage(voltage)
+	}
 
 	fun setAngleMotorVoltage(voltage: Volts) {
 		angleMotor.setVoltage(voltage)
-	}
-
-	fun setWheelsMotorVoltage(voltage: Volts) {
-		wheelsMotor.setVoltage(voltage)
-	}
-
-	fun setAngleMotorSetpoint(setpoint: Rotation2d) {
-		if (setpoint.degrees in 0.0..180.0) {
-			angleSetpoint = setpoint
-		} else {
-			edu.wpi.first.wpilibj
-				.Alert("New jointed elevator height setpoint not in motion range. Value not updated.", AlertType.kError)
-				.set(true)
-			DriverStation.reportWarning("New climbing setpoint of ${setpoint.degrees} degrees is not in the range of motion.", true)
-		}
-		val output = anglePIDController.calculate(currentAngle.radians, angleSetpoint.radians)
-		if (!isMovingTowardsAngleLimits(output)) {
-			angleMotor.setVoltage(output + calculateAngleMotorFF())
-		} else {
-			angleMotor.setVoltage(calculateAngleMotorFF())
-		}
-	}
-
-	// --- Telemetry ---
-
-	override fun initSendable(builder: SendableBuilder) {
-		builder.addBooleanProperty("Is within angle tolerance", { isWithinAngleTolerance }, null)
 	}
 }
