540c7b63eb51a3e14e735979d392bf73ca2ecdcf
Alon Gal <alon.gal.kufsa@gmail.com>
2025-03-08 20:13:14 +0200

    Fixed coral vision error

--- src/main/kotlin/frc/robot/vision/CoralVision.kt
+++ src/main/kotlin/frc/robot/vision/CoralVision.kt
@@ -1,13 +1,18 @@
 package frc.robot.vision
 
-import com.hamosad1657.lib.units.degrees
+import com.hamosad1657.lib.units.Length
 import com.hamosad1657.lib.vision.HaPhotonCamera
 import edu.wpi.first.math.geometry.Rotation2d
+import edu.wpi.first.wpilibj.DriverStation
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import org.photonvision.targeting.PhotonPipelineResult
 import org.photonvision.targeting.PhotonTrackedTarget
 import kotlin.math.absoluteValue
 
 object CoralVision {
+	val CAMERA_HEIGHT: Length = Length.fromMeters(0.94)
+	val CAMERA_ANGLE: Rotation2d = Rotation2d.fromDegrees(60.0)
+
 	val CORAL_INTAKING_YAW = Rotation2d.fromDegrees(-13.6)
 	val CORAL_INTAKING_YAW_TOLERANCE = Rotation2d.fromDegrees(3.5)
 
@@ -24,7 +29,13 @@ object CoralVision {
 		it < CORAL_INTAKING_YAW_TOLERANCE.degrees && it != 0.0
 	}
 
-	val coralPitch: Rotation2d get() = Rotation2d.fromDegrees((bestTarget?.getPitch() ?: 0.0.degrees) as Double)
+	val coralPitch: Rotation2d get() = Rotation2d.fromDegrees(bestTarget?.getPitch() ?: 0.0)
 
-	val coralSkew: Rotation2d get() = Rotation2d.fromDegrees((bestTarget?.getSkew() ?: 0.0.degrees) as Double)
+	val coralSkew: Rotation2d get() = Rotation2d.fromDegrees(bestTarget?.getSkew() ?: 0.0)
+
+	fun calculateXToCoral(): Length {
+		return Length.fromMeters(CAMERA_HEIGHT.asMeters * ((coralPitch.rotateBy(CAMERA_ANGLE)).tan)).also {
+			SmartDashboard.putNumber("X to coral", it.asMeters)
+		}
+	}
 }
